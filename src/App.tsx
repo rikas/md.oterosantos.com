9b3071adcd5645301180c10216167e370caa08f4
Ricardo Otero <oterosantos@gmail.com>
2026-01-08 10:19:31 +0000

    Update title

--- src/App.tsx
+++ src/App.tsx
@@ -60,9 +60,10 @@ export default function App(): React.ReactElement {
     <div className="container m-auto py-10 px-4">
       <div className="flex flex-col gap-10">
         <div className="flex flex-col gap-2 items-start">
-          <h1 className="text-3xl md:text-6xl font-headings opacity-80">
-            My Mega Drive Collection
+          <h1 className="bg-clip-text bg-linear-to-br from-blue-100 via-blue-500 to-emerald-400 text-transparent text-3xl md:text-6xl font-headings opacity-80">
+            MegaDrive Database
           </h1>
+
           <h5 className="text-lg md:text-2xl text-slate-500 font-extralight">
             <span className="font-extralight">{percentageComplete}% Complete</span> (
             {ownedGames.length} / {games.length})
